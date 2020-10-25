b0ba760e1f87e5a7ccdb5ca83ea16411ee63fe83
sean <seanparker500@gmail.com>
2020-10-24 20:58:50 -0400

    Move url request job setup to new '/start' view

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import requests
 import operator
 import re
 import nltk
+import json
+
 from flask import Flask, render_template, request, jsonify
 from extensions import db
 from stop_words import stops
@@ -74,22 +76,27 @@ def count_and_save_words(url):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    results = {}
-    if request.method == "POST":
-        # this import solves a rq bug which currently exists
-        from app import count_and_save_words
+    return render_template('index.html')
 
-        # get url that the person has entered
-        url = request.form['url']
-        if not url[:8].startswith(('https://', 'http://')):
-            url = 'http://' + url
-        job = q.enqueue_call(
-            func=count_and_save_words, args=(url,), result_ttl=5000
-        )
-        print(job.get_id())
-        sys.stdout.flush()
 
-    return render_template('index.html', results=results)
+@app.route('/start', methods=['POST'])
+def get_counts():
+    # this import solves a rq bug which currently exists
+    from app import count_and_save_words
+
+    # get url that the person has entered
+    data = json.loads(request.data.decode())
+    url = data['url']
+    if not url[:8].startswith(('https://', 'http://')):
+        url = 'http://' + url
+    job = q.enqueue_call(
+        func=count_and_save_words, args=(url,), result_ttl=5000
+    )
+
+    # return created job id
+    return job.get_id()
+
+
 
 
 @app.route('/results/<job_key>', methods=['GET'])
